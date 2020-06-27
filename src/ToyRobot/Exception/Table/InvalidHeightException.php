2290ef288d4e437ce11800bfcb454a8875c03a98
Carl Borgas <carlborgas@gmail.com>
2020-06-27 17:58:32 +1000

    Completed table data structure with tests
    - Included exceptions for negative height and widths

---
+++ src/ToyRobot/Exception/Table/InvalidHeightException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ToyRobot\Exception\Table;
+
+class InvalidHeightException extends \InvalidArgumentException
+{
+
+}
--- tests/ToyRobot/Unit/TableTest.php
+++ tests/ToyRobot/Unit/TableTest.php
@@ -2,15 +2,55 @@
 
 namespace ToyRobot\Unit;
 
+use ToyRobot\Table;
+
 class TableTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @test
      */
-    public function table_is_instantiable()
+    public function table_is_instantiable(): void
     {
-        $table = new \ToyRobot\Table;
+        $table = new Table;
+        $this->assertInstanceOf(Table::class, $table);
+    }
 
-        $this->assertInstanceOf(\ToyRobot\Table::class, $table);
+    /**
+     * @test
+     */
+    public function can_get_and_set_table_height(): void
+    {
+        $table = new Table;
+        $table->setHeight(10);
+        $this->assertEquals(10, $table->getHeight());
+    }
+    /**
+     * @test
+     */
+    public function can_get_and_set_table_width(): void
+    {
+        $table = new Table;
+        $table->setWidth(20);
+        $this->assertEquals(20, $table->getWidth());
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_set_table_height_to_negative_integer(): void
+    {
+        $table = new Table;
+        $this->expectException(\ToyRobot\Exception\Table\InvalidHeightException::class);
+        $table->setHeight(-3);
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_set_table_width_to_negative_integer(): void
+    {
+        $table = new Table;
+        $this->expectException(\ToyRobot\Exception\Table\InvalidWidthException::class);
+        $table->setWidth(-4);
     }
 }
