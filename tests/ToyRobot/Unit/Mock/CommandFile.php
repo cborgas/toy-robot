db68161c25170e6cab2df5d4e13599dbb877ae79
Carl Borgas <carlborgas@gmail.com>
2020-06-30 21:12:06 +1000

    Improve text file reader coverage

--- tests/ToyRobot/Unit/Mock/CommandFile.php
+++ tests/ToyRobot/Unit/Mock/CommandFile.php
@@ -18,10 +18,14 @@ class CommandFile
         $this->temporaryFileName = tempnam("/tmp", "toy-robot-commands");
 
         $handle = fopen($this->temporaryFileName, "w");
+        $countCommands = count($commands);
 
-        foreach ($commands as $command) {
+        foreach ($commands as $key => $command) {
+            // Only append PHP_EOL on all but last command
+            $command = $countCommands == $key + 1 ? $command : $command . PHP_EOL;
+
             // Write each command on a new line to the temporary file
-            fwrite($handle, $command . PHP_EOL);
+            fwrite($handle, $command);
         }
 
         fclose($handle);
